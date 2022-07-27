69a0d7cdd1c14b4e43145debc6f0bf8be0f8673c
Yanal Yahya <yanal.alyhya@gmail.com>
2022-07-27 12:45:32 +0300

    move generate password function to utils - RAZRABOTKA-980

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+import { authValidations } from 'enums/authorization';
 import { UrlBreadcrumd } from 'models/types';
 
 export const getUrlAdress = (addres: string): UrlBreadcrumd[] => {
@@ -21,3 +22,18 @@ export const getParameterFromUrl = (param: string): string | null => {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const scrollTopPage = () => window.scrollTo(0, 0);
+
+export const generatePassword = (
+  length = Math.floor(Math.random() * (8 - 6 + 1)) + 6,
+  wishlist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
+) => {
+  const password = Array.from(crypto.getRandomValues(new Uint32Array(length)))
+    .map((x) => wishlist[x % wishlist.length])
+    .join('');
+
+  if (!authValidations.password.pattern.regex.test(password)) {
+    generatePassword();
+  }
+  
+  return password;
+};
